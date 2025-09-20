5fe4df5f3f7461a83006e78cefd2532e5c2877b9
daniel <danemokhor@gmail.com>
2025-09-20 13:57:06 +0100

    Updated env to switch between local db and production db

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -13,11 +13,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import environ
+
+#default values
+env = environ.Env(
+    DEBUG= (bool, False),
+    USE_LOCAL_DB=(bool, True)
+)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -82,14 +90,24 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
-DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+if (env('USE_LOCAL_DB')):
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
+
+else:
+    DATABASES= {
+        "default": env.db()
+    }
+
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
